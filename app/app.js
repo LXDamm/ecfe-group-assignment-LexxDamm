7162e367ca287243e2e47001840821c2cef6d566
Lexx Utopia Olivia Damm <lexx.damm@gmail.com>
2020-12-14 13:09:00 +0100

    Works

--- app/app.js
+++ app/app.js
@@ -1,13 +1,39 @@
+//import Data from './data.js';
 import UI from './ui.js';
 
 class App {
     constructor() {
-        this.json;
-        this.UI = new UI();
+        this.questions = [];
+        this.currentQuestion = 0;
+        this.ui = new UI();
+    }
+    fetchQuiz() {
+        let self = this;
+        fetch('./app/questions.json')
+        .then(function(response) {
+            if (response.status !== 200) {
+                console.log('Looks like there was a problem. Status Code: ' + response.status);
+                return;
+            }
+            response.json().then(function(json) {
+                self.questions = json.questions;
+                document.dispatchEvent(new Event('fetchDone'));    
+            });
+        })
+        .catch(function(err) {
+            console.log('Fetch Error :-S', err);
+        });
+    }
+    addEventListeners() {
+        const self = this;
+        document.addEventListener('fetchDone', (event) => {
+            console.log(self.questions[this.currentQuestion]);
+            self.ui.renderQuestion(self.questions[this.currentQuestion]);
+        });
     }
     run() {
-        fetch()
-        render()
+        this.addEventListeners();
+        this.fetchQuiz();
     }
 }
 
--- app/ui.js
+++ app/ui.js
@@ -1,15 +1,17 @@
-import Data from "./data";
-
-import Data from './data.js';
-
 class UI {
     constructor() {
-        this.data = new Data();
+        this.questionTitleE = document.getElementById('questionTitle');
+        this.answerOneE = document.getElementById('cardText-1');
+        this.answerTwoE = document.getElementById('cardText-2');
+        this.answerThreeE = document.getElementById('cardText-3');
+        this.answerFourE = document.getElementById('cardText-4');
     }
-    render() {
-        this.data.fetchQuiz().then((data) => {
-            
-        });
+    renderQuestion(question) {
+        this.questionTitleE.innerText = question.questionString;
+        this.answerOneE.innerText = question.possibleAnswers[0];
+        this.answerTwoE.innerText = question.possibleAnswers[1];
+        this.answerThreeE.innerText = question.possibleAnswers[2];
+        this.answerFourE.innerText = question.possibleAnswers[3];
     }
 }
 
